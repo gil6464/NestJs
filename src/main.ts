9feb71c06d3a0bdc585d5d44bf984cf590cfb1f2
gil6464 <gil6464@gmail.com>
2024-08-08 00:34:33 +0300

    Add scripts to restart db and migrate in package json, deleted dotenv, using nest config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
